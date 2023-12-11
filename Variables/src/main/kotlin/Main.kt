cc3f332724fd192299028c5f0aa753190d5db6a2
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-11 14:40:06 +0600

    learning about var and val in kotlin

---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+//    the naming convention is camel casing in kotlin
+//    variable declaration in kotlin
+    var userName: String = "Rahat";
+//    replacing the value
+    userName = "Akash";
+    println("Variables $userName");
+//    when we declare val we can not replace the value over time.
+    val userOne: String = "Rahat";
+
+    println("the name of val $userOne");
+}
