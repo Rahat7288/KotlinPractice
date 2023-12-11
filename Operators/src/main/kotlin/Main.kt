9bdf09cc5abb74515b18a9809f3f16da9eb94617
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-12 00:58:21 +0600

    functions in kotlin"

--- Operators/src/main/kotlin/Main.kt
+++ Operators/src/main/kotlin/Main.kt
@@ -1,23 +1,53 @@
-fun main() {
-    println("==========Control Flow=================");
+fun main(args: Array<String>) {
+//    println("==========Control Flow=================");
+//
+////    if else statements=================
+//
+//    val isActive = false;
+//
+//    if (isActive ){
+//        println("this line is active!");
+//    }
+//    else{
+//        println("THis line is inActive");
+//    }
+//
+////    null value
+//
+//    var text: String? = "Rahat";
 
-//    if else statements=================
+//    calling function
+    println("Function calling");
 
-    val isActive = false;
+sayHello();
+    sayBye();
+    withName("Rahat");
 
-    if (isActive ){
-        println("this line is active!");
-    }
-    else{
-        println("THis line is inActive");
-    }
-
-//    null value
-
-    var text: String? = "Rahat";
-
-
+//    var result: Int = getMax(a = 5, b = 3);
+//    println(" the Result is $result");
 
 
 
 }
+
+fun sayHello(){
+    println("Hello world!");
+}
+
+fun sayBye(){
+    println("Say Bye");
+}
+//passing data to function
+fun  withName(name: String){
+    println("Hello $name");
+}
+
+//return in function
+//fun getMax(a: Int, b: Int ): Int{
+//    val max = if(b <= a) {
+//        a
+//    } else  b;
+//
+//    return max;
+//}
+
