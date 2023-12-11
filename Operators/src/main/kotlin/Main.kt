81365cc55e8d0de3c31efa2b050f4d9637ff6442
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-12 00:29:08 +0600

    operators and null values in kotlin

--- Operators/src/main/kotlin/Main.kt
+++ Operators/src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
-    println("Hello World!")
+    println("==========Control Flow=================");
+
+//    if else statements=================
+
+    val isActive = false;
+
+    if (isActive ){
+        println("this line is active!");
+    }
+    else{
+        println("THis line is inActive");
+    }
+
+//    null value
+
+    var text: String? = "Rahat";
+
+
+
+
+
 }
