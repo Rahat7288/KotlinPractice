7a283b31e0b3df8146f284ab2f2b6b1ad446271a
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-13 20:28:30 +0600

    learning function over loading

--- TestFunctions/src/Main.kt
+++ TestFunctions/src/Main.kt
@@ -4,6 +4,8 @@ fun main() {
     sayHello("Rahat")
 
     println(getMax(4,5))
+//    over loading function
+    println(getMax(a = 5.6, b = 6.1))
 }
 
 fun sayHello(name: String){
@@ -14,4 +16,12 @@ fun sayHello(name: String){
 fun getMax(valeOne: Int, valueTwo: Int): Int{
     val max = if (valeOne > valueTwo) valeOne else valueTwo
     return  max
+}
+
+//function overloading
+
+fun getMax(a: Double, b: Double): Double{
+    val max = if(a > b) a else b
+
+    return max
 }
