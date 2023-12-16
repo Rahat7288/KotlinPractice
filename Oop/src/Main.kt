63b80c2514e77394c569c14966ff4bf0b6bebc0a
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-16 23:47:13 +0600

    making a class and definign an objects for the class

--- Oop/src/Main.kt
+++ Oop/src/Main.kt
@@ -1,3 +1,37 @@
 fun main() {
-    println("Hello World!")
+    println("=======learning OOP================")
+//    defining object of the car
+    val car1 = Car()
+    car1.name = "Tesla"
+    car1.model = "S Plaid"
+    car1.color = "Red"
+    car1.doors = 4
+
+    println("Name = ${car1.name}")
+    println("Name = ${car1.model}")
+    println("Name = ${car1.color}")
+    println("Name = ${car1.doors}")
+
+    car1.move()
+    car1.stop()
+
+
+}
+
+//class define==========
+
+class Car{
+//    properties==========
+    var name = ""
+    var model = ""
+    var color = ""
+    var doors = 0
+
+    fun move(){
+        println("The car $name is moving ")
+
+    }
+    fun  stop(){
+        println("The car $name has stopped")
+    }
 }
