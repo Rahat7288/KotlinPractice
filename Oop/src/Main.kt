6c47cfe5490a800039ac0b7ccfa29ac24ac8a7a6
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-20 00:27:50 +0600

    worrking on oop

--- Oop/src/Main.kt
+++ Oop/src/Main.kt
@@ -21,21 +21,3 @@ fun main() {
 //class define==========
 //defining primary constructor
 
-class Car(//    properties==========
-    var name: String, var model: String, var color: String, var doors: Int
-){
-
-    fun move(){
-        println("The car $name is moving ")
-
-    }
-    fun  stop(){
-        println("The car $name has stopped")
-    }
-}
-
-//when to use property and when to use constructor
-/*
-* if we need to do some operations on a property then we will use property other than we will
-* use constructor
-* */
