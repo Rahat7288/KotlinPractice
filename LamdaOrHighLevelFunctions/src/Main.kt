e3f5dd5b51973bf88cbd979c1c5c2b773e4fdc86
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-23 02:33:16 +0600

    learning about the lamda function

--- LamdaOrHighLevelFunctions/src/Main.kt
+++ LamdaOrHighLevelFunctions/src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
     println("===========Lamda And high level function===========")
+    val myLamda = {a: Int -> println(a) }
+    add(5,5, myLamda)
+    println("Declearing the lamda function")
+
+}
+
+fun add (a: Int, b: Int, action: (Int)-> Unit){
+    action(a+b)
 }
