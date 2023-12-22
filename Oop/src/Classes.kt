1224af4a9d1e9cd610ca2b9cb528f5ce2603c125
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-22 16:32:19 +0600

    singleton learning and practice

--- Oop/src/Classes.kt
+++ Oop/src/Classes.kt
@@ -21,3 +21,72 @@ class Car(//    properties==========
 *
 * in kotlin we can define multiple constructor known as secondary constructor
 * */
+
+class User ( firstName: String, var lastName: String, var age: Int){
+    var firstName: String
+    lateinit var middleName: String
+
+    init {
+        if (firstName.lowercase().startsWith(prefix = "a")){
+            this.firstName = firstName
+        }else{
+            this.firstName = "User"
+            println("The first name doesn't start with A or a")
+        }
+    }
+}
+
+//lateinit ======================
+
+
+
+
+//companion object
+/*
+with the help of companion object we don't need to create an object to access of the
+specific class.
+companion object itself a property of the specific class
+
+/
+ */
+
+class Calculator(){
+    companion object{
+        fun sum (a: Int, b: Int): Int{
+            return  a+ b;
+        }
+    }
+
+}
+//companion object =========================
+
+//Singleton
+/*
+* if we don't want to create multiple instance of a class we will use Singleton
+
+*
+*
+* with a singleton one operation will execute only one time
+* it will not do the same work multiple time
+* */
+
+//class Database private  constructor (){
+//
+//
+//    companion object{
+//        private var instance: Database? = null
+//        fun getInstance(): Database?{
+//            if (instance == null){
+//                instance = Database()
+//            }
+//            return  instance
+//        }
+//    }
+//}
+
+object  Database {
+    init {
+        println("Database created")
+    }
+}
+//singleton =======================
--- Oop/src/Main.kt
+++ Oop/src/Main.kt
@@ -1,66 +1,17 @@
 fun main() {
     println("=======learning OOP================")
-////    defining object of the car
-//    val car1 = Car("Tesla", "S Plade", color = "Red", 4)
-////    car1.name = "Tesla"
-////    car1.model = "S Plaid"
-////    car1.color = "Red"
-////    car1.doors = 4
-//
-//    println("Name = ${car1.name}")
-//    println("Name = ${car1.model}")
-//    println("Name = ${car1.color}")
-//    println("Name = ${car1.doors}")
-//
-//    car1.move()
-//    car1.stop()
 
-    val  user =  User("Rahat","Akash",26)
-    val  user2 = User("Akash","Rahat", 26)
+    println("=======printing singleton=========")
 
-    println("The User Name is ${user.firstName}")
-
-
-    println("companion objects")
-
-
-
-    val result = Calculator.sum(5,10)
-    println("the result is : $result")
+    println(Database)
 
 
 
 }
 
-class User ( firstName: String, var lastName: String, var age: Int){
-    var firstName: String
-    lateinit var middleName: String
 
-    init {
-        if (firstName.lowercase().startsWith(prefix = "a")){
-            this.firstName = firstName
-        }else{
-            this.firstName = "User"
-            println("The first name doesn't start with A or a")
-        }
-    }
-}
 
-//companion object
-/*
-with the help of companion object we don't need to create an object to access of the
-specific class.
-companion object itself a property of the specific class
 
-/
- */
 
-class Calculator(){
-    companion object{
-        fun sum (a: Int, b: Int): Int{
-            return  a+ b;
-        }
-    }
 
-}
 
