facd8f8cec2e153e09c3da645e6f56b2fd26d826
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-23 18:03:00 +0600

    more about coroutine

--- KotlinCoroutine/src/main/kotlin/Main.kt
+++ KotlinCoroutine/src/main/kotlin/Main.kt
@@ -22,9 +22,21 @@ fun main() {
 //        println("fake work finishd : ${Thread.currentThread().name}")
 //    }
    val parentJob =  CoroutineScope(Default).launch{
-        println("fake work starts: ${Thread.currentThread().name}")
-        delay(2000)
-        println("fake work ends: ${Thread.currentThread().name}")
+//        println("fake work starts: ${Thread.currentThread().name}")
+//        delay(2000)
+//        println("fake work ends: ${Thread.currentThread().name}")
+
+       val job1 = launch {
+          val result1 = getDetail(Thread.currentThread().name)
+           println("form coroutine 1:  $result1")
+       }
+//       if we want to run the job in sequential not parallel order
+//       we will call join
+       job1.join()
+       val job2 = launch {
+           val result2 = getData2(Thread.currentThread().name)
+           println("form coroutine 2: $result2")
+       }
     }
     runBlocking {
         parentJob.join()
@@ -34,4 +46,18 @@ fun main() {
 
 
     println("Main program ends: ${Thread.currentThread().name}")
+}
+
+private  suspend fun getDetail(threadName: String): String{
+    println("Fake work1 starts: $threadName")
+    delay(2000)
+    println("Fake work1 ends: $threadName")
+    return  "Result 1"
+}
+
+private suspend fun getData2(threadName: String): String{
+    println("Fake work 2 starts : $threadName")
+    delay(2000)
+    println("Fake work 2 ends : $threadName")
+    return "Fake Work 2"
 }
