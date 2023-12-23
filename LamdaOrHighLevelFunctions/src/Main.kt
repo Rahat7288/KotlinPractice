f3024fd464bfabea76f8529f1023aedd469b7c42
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-12-23 13:33:41 +0600

    lamda function with it keyword

--- LamdaOrHighLevelFunctions/src/Main.kt
+++ LamdaOrHighLevelFunctions/src/Main.kt
@@ -4,8 +4,18 @@ fun main() {
     add(5,5, myLamda)
     println("Declearing the lamda function")
 
+    upperCase("hello",{s: String, -> s.uppercase()})
+//    if we have only one parameter we can use it
+    upperCase("rahat", {it.uppercase()})
+
 }
 
 fun add (a: Int, b: Int, action: (Int)-> Unit){
     action(a+b)
+}
+
+fun upperCase(str: String, myFunction: (String)-> String){
+    val uppercasedWord = myFunction(str)
+
+    println(uppercasedWord)
 }
